80e4c51670a0cd5f43f923840fbce651b1f59d3f
Harry <>
2022-02-16 14:12:29 +0800

    Trackable Scroll View

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,11 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var contentOffset = CGFloat(0)
+    
     var body: some View {
         NavigationView {
             ZStack(alignment: .top) {
-                Text("Hello, world!")
-                    .padding()
+                TrackableScrollView { offset in
+                    withAnimation {
+                        contentOffset = offset.y
+                    }
+                    print(contentOffset)
+                } content: {
+                    Text("Hello, world!")
+                        .padding()
+                }
+                
+                VisualEffectBlur(blurStyle: .systemMaterial)
+                    .opacity(contentOffset < -16 ? 1: 0)
+                    //.animation(.easeIn)
+                    .ignoresSafeArea()
+                    .frame(height: 0)
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
             .background(AccountBackground())
